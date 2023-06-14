907ca39f680680429b89869ccf43f10912938a75
truefibot <truebot.github@trusttoken.com>
2023-06-14 10:35:00 +0000

    🦤 Handle solc compiler download manually on CI (#1363)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,31 @@ import 'hardhat-gas-reporter'
 import mocharc from './.mocharc.json'
 import compiler from './.compiler.json'
 
+import { subtask } from 'hardhat/internal/core/config/config-env'
+import { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } from 'hardhat/builtin-tasks/task-names'
+import path from 'path'
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const isCI = process.env.CI
+
+// Override for CI to use downloaded solc compiler
+// Based on: https://github.com/NomicFoundation/hardhat/issues/1639#issuecomment-876291261
+subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, (args: any, hre: any, runSuper: any) => {
+  if (isCI && args.solcVersion === '0.8.16') {
+    const compilerPath = path.join(__dirname, '..', '..', '..', 'tools-cache', 'hardhat', 'solc-v0.8.16')
+
+    return {
+      compilerPath,
+      isSolcJs: false,
+      version: args.solcVersion,
+      longVersion: '0.8.16',
+    }
+  }
+
+  // we just use the default subtask if the version is not 0.8.5
+  return runSuper()
+})
+
 module.exports = {
   paths: {
     sources: './contracts',
